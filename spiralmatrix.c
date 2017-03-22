84f95a100871fa773fbf3be356292671d2a8df6f
ambrishsharma <ambrishsharma1996@gmail.com>
2017-03-22 20:55:29 +0530

    Create spiralmatrix.c

---
+++ spiralmatrix.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+ int main()
+{
+	int a[10][10],i,j,n,x=1;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(i%2==1)
+			{
+				a[i][j]=x;
+				x++;
+			}
+			else
+			{
+				a[i][n+1-j]=x;
+				x++;
+			}
+		}
+	}
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			printf("\t%d",a[i][j]);
+		}
+		printf("\n");
+	}
+}
